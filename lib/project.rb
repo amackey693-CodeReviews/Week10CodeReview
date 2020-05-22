171b5623eacc5416ba9eb3a550f8d18847158e70
Allison Mackey <a.mackey693@gmail.com>
2020-05-22 09:19:33 -0700

    add logic to pass #project test on #delete, test PASSES

--- lib/project.rb
+++ lib/project.rb
@@ -40,5 +40,9 @@ class Project
     @title = title
     DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+  end
 end
 
