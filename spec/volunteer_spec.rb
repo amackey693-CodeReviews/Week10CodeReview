379862f71960dc389f119e1c34a28a648621899b
Allison Mackey <a.mackey693@gmail.com>
2020-05-22 09:37:00 -0700

    test on #volunteer spec .find, expect test to FAIL

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -43,7 +43,15 @@ describe Volunteer do
     end
   end
 
-
+  describe '.find' do
+    it 'returns a volunteer by id' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+      volunteer2.save
+      expect(Volunteer.find(volunteer1.id)).to eq volunteer1
+    end
+  end
 end
  
 
@@ -54,13 +62,3 @@ end
 
 
 
-  # describe '.find' do
-  #   it 'returns a volunteer by id' do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
-  #     volunteer2.save
-  #     expect(Volunteer.find(volunteer1.id)).to eq volunteer1
-  #   end
-  # end
-# end
