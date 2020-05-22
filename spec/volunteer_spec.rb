12728404b58734d99735bf5263a6e0b640858595
Allison Mackey <a.mackey693@gmail.com>
2020-05-22 09:31:03 -0700

    test on #volunteer spec #save, expect test to FAIL

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -27,6 +27,15 @@ describe Volunteer do
     end
   end
 
+  describe '#save' do
+    it 'adds a volunteer to the database' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      expect(Volunteer.all).to eq [volunteer1]
+    end
+  end
+
+
 end
  
 
@@ -43,13 +52,6 @@ end
   #   end
   # end
 
-  # describe '#save' do
-  #   it 'adds a volunteer to the database' do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     expect(Volunteer.all).to eq [volunteer1]
-  #   end
-  # end
 
   # describe '.find' do
   #   it 'returns a volunteer by id' do
